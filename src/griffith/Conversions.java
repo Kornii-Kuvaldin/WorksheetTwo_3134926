3646ab0e37e55a1535719be0b297ec30ca355b6d
Kornii Kuvaldin <kornii.kuvaldin@student.griffith.ie>
2025-03-05 10:38:50 +0000

    Added method stubs

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,5 +1,19 @@
 package griffith;
 
 public class Conversions {
-
+	public double euroToDollar(double euro) {
+		return -1;
+	}
+	public double dollarToEuro(double dollar) {
+		return -1;
+	}
+	public int stringToInteger (String val) {
+		return -1;
+	}
+	public String integerToString (int val) {
+		return "";
+	}
+	public String switchCase() {	// change uppercase to lowercase and vice versa
+		return "";
+	} 
 }
