911f0590b0981e3977119cdfe7f175e1cf8c0136
Kornii Kuvaldin <kornii.kuvaldin@student.griffith.ie>
2025-03-07 16:29:27 +0000

    StringToInteger implementation

--- src/griffith/Conversions.java
+++ src/griffith/Conversions.java
@@ -1,15 +1,34 @@
 package griffith;
 
 public class Conversions {
+	final double RATE = 1.07; //This constant is the rate of exchange for euros to dollars
+	
 	public double euroToDollar(double euro) {
-		return -1;
+		return RATE*euro;
 	}
+	
 	public double dollarToEuro(double dollar) {
-		return -1;
+		return dollar/RATE;
 	}
+	
 	public int stringToInteger (String val) {
-		return -1;
+		int multiplier = 1; //This is used to determine if the number is negative or positive
+		int power = val.length()-1; //This is needed for multiplying powers of 10 to each individual number
+		int total = 0;
+	if(!val.matches("^[-]?[0-9]*$") || val.isEmpty())
+		throw new IllegalArgumentException("String can't be empty or of incorrect format");
+		for(char i: val.toCharArray()) {
+		if(i == '-') {
+			multiplier = -1;
+			power--;
+			continue;
+		}
+		total+= (i-'0')*(Math.pow(10, power)); //To get the value of a number from an ascii table		
+		power--;
+		}
+		return total*multiplier;
 	}
+	
 	public String integerToString (int val) {
 		return "";
 	}
--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -1,5 +1,6 @@
 package griffith;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -7,8 +8,29 @@ import org.junit.jupiter.api.Test;
 class ConversionsTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testEuroToDollar() {
+		Conversions conversions = new Conversions();
+		assertEquals(107, conversions.euroToDollar(100)); //Positives
+		assertEquals(0, conversions.euroToDollar(0));	//Zero
+		assertEquals(-107, conversions.euroToDollar(-100));	//Negatives
 	}
 
+	@Test
+	void testDollarToEuro() {
+		Conversions conversions = new Conversions();
+		assertEquals(100, conversions.dollarToEuro(107)); //Positives
+		assertEquals(0, conversions.dollarToEuro(0));	//Zero
+		assertEquals(-100, conversions.dollarToEuro(-107));	//Negatives
+	}
+
+	@Test
+	void testStringToInteger() {
+		Conversions conversions = new Conversions();
+		assertEquals(42, conversions.stringToInteger("42"));
+		assertEquals(-42, conversions.stringToInteger("-42"));
+		Throwable e = assertThrows(IllegalArgumentException.class, () ->
+			conversions.stringToInteger(""));
+			assertEquals("String can't be empty or of incorrect format", e.getMessage());
+	}
+	
 }
